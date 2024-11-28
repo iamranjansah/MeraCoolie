dd6837284383092526a0a68af3fcf07033bb1f11
nraj07054 <nraj07054@gmail.com>
2024-11-28 22:27:20 +0530

    Remove unnecessary files

--- backend/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createUser } = require('../controllers/userController');
-const router = express.Router();
-
-// Register a new user
-router.post('/register', createUser);
-
-module.exports = router;
